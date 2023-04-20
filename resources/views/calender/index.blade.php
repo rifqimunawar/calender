eb9b00afbf0e34c2fce96c5654c85962a50ec27d
rifqimunawar <rifqimunawar48@gmail.com>
2023-04-21 02:35:35 +0700

    update header calender

--- resources/views/calender/index.blade.php
+++ resources/views/calender/index.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
  <head>
-  <title>Jquery Fullcalandar Integration with PHP and Mysql</title>
+  <title>Calender</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
@@ -15,10 +15,9 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <h3 class="text-center"> <br> Calender</h3>
         <div class="col-md-12 offset-1 mx-4 mb-5">
           
-          <h2 align="center"><a href="#">Jquery Fullcalandar Integration with PHP and Mysql</a></h2>
+          <h2 align="center"><br><a href="#">Fullcalandar Integration</a></h2>
           <br />
           <div class="container">
             <div id="calendar"></div>
@@ -31,8 +30,12 @@
 
   <script>
     $(document).ready(function() {
-      var calendar = $('#calendar').fullCalendar({
-  
+      $('#calendar').fullCalendar({
+        header:{ //membuat tulisan fitur yang ada di header 
+          left:'prev, next today', //tombol dan hri sebelah kiri
+          center: 'title', //judul dan bulan
+          right:'month, agendaWeek, agendaDay', //tombol di kanan
+        }
       });
     });
     </script>
